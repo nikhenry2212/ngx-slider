2f0e4575c47383905ead57522ecae8ab88ba1483
nikolas <nikolaspg74@gmail.com>
2021-12-30 14:11:55 -0300

    fix: valueChange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Options, LabelType } from '@angular-slider/ngx-slider';
+import { FormControl, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -8,36 +9,43 @@ import { Options, LabelType } from '@angular-slider/ngx-slider';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  sliderForm: FormGroup = new FormGroup({
+    sliderControl: new FormControl([20, 80])
+  });
 
-constructor() {
-  
-}
+  constructor() { }
   objeto = [
-    {AplicacaoInicial: '100000'},
-    {AplicacaoInicial: '1'},
-    {AplicacaoInicial: '200000000'},
-    {AplicacaoInicial: '3000000000'},
-    {AplicacaoInicial: '555555555'},
+    { AplicacaoInicial: '100000', img: 'https://image.freepik.com/fotos-gratis/pessoas-torcendo-na-festa-de-ano-novo_23-2148724240.jpg' },
+    { AplicacaoInicial: '1', img: "https://image.freepik.com/fotos-gratis/especialista-em-ti-verificando-o-codigo-no-computador-no-escritorio-escuro-a-noite_1098-18699.jpg" },
+    { AplicacaoInicial: '200000000', img: "https://image.freepik.com/fotos-gratis/codificadores-de-ti-trabalhando-no-laptop-no-escritorio-em-pe-a-mesa_158595-5240.jpg" },
+    { AplicacaoInicial: '3000000000', img: "https://image.freepik.com/fotos-gratis/conceito-de-graficos-de-conexao-de-tecnologia-da-informacao_53876-121040.jpg" },
+    { AplicacaoInicial: '555555555', img: "https://image.freepik.com/fotos-gratis/tela-de-digitalizacao-de-programador-em-seu-smartwatch-com-camera-do-smartphone_1098-18710.jpg" },
   ]
+
   title = 'ngx-slider';
-  minValue: number = 100;
-  maxValue: number = 400;
+  minValue: number = 0;
+  maxValue: number = 0;
   options: Options = {
     floor: 0,
     ceil: 500,
     translate: (value: number, label: LabelType): string => {
       switch (label) {
         case LabelType.Low:
-          return '<b>Min price:</b> $' + value;
+          return '<b>Min :</b> $' + value;
         case LabelType.High:
-          return '<b>Max price:</b> $' + value;
+          return '<b>Max :</b> $' + value;
         default:
           return '$' + value;
       }
     }
   };
 
-  ngOnInit(){
+  filter(event: any) {
+    this.objeto.map(event => console.log(event)
+    )
+  }
 
+  ngOnInit() {
+   
   }
 }
